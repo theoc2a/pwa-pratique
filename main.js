73dc970fcc46a2a06006f933a9362d61e17bb6ec
theoc2a <theocussac2a@gmail.com>
2020-09-24 14:18:03 +0200

    3. Service Worker - résultat

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+console.log('hello depuis main');
 const technosDiv = document.querySelector('#technos');
 
 function loadTechnologies(technos) {
@@ -22,3 +23,9 @@ function loadTechnologies(technos) {
 }
  
 loadTechnologies(technos);
+
+if(navigator.serviceWorker) {
+    navigator.serviceWorker
+        .register('sw.js')
+        .catch(err => console.error('service worker NON enregistré', err));
+}
