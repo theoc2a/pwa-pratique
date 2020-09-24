ce9a2cc74d8f364abb2901e8792afdec499d2814
theoc2a <theocussac2a@gmail.com>
2020-09-24 13:48:55 +0200

    1. et 2. Initialisation - résultat

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-console.log('hello depuis main');
 const technosDiv = document.querySelector('#technos');
 
 function loadTechnologies(technos) {
@@ -6,7 +5,14 @@ function loadTechnologies(technos) {
         .then(response => {
             response.json()
                 .then(technos => {
-                    const allTechnos = technos.map(t => `<div><b>${t.name}</b> ${t.description}  <a href="${t.url}">site de ${t.name}</a> </div>`)
+                    const allTechnos = technos.map(t => `
+                    <div class="card">
+                        <div class="card-body">
+                        <h5 class="card-title">${t.name}</h5>
+                        <p class="card-text">${t.description}</p>
+                        <a href="${t.url}" class="card-link">site de ${t.name}</a>
+                        </div>
+                    </div>`)
                             .join('');
             
                     technosDiv.innerHTML = allTechnos; 
@@ -14,5 +20,5 @@ function loadTechnologies(technos) {
         })
         .catch(console.error);
 }
-
+ 
 loadTechnologies(technos);
