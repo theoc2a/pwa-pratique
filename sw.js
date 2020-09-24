80182e0f5b58092ba7f288883ab2ff1fd6a9a073
theoc2a <theocussac2a@gmail.com>
2020-09-24 15:59:40 +0200

    4. Gestion du cache - résultat

--- sw.js
+++ sw.js
@@ -1,7 +1,27 @@
 console.log("hello depuis le service worker");
 	
+const cacheName = 'veille-techno' + '1.1';
+ 
 self.addEventListener('install', (evt) => {
     console.log(`sw installé à ${new Date().toLocaleTimeString()}`);
+ 
+    const cachePromise = caches.open(cacheName).then(cache => {
+        return cache.addAll([
+            'index.html',
+            'main.js',
+            'style.css',
+            'vendors/bootstrap4.min.css',
+            'add_techno.html',
+            'add_techno.js',
+            'contact.html',
+            'contact.js',
+        ])
+        .then(console.log('cache initialisé'))
+        .catch(console.err);
+    });
+ 
+    evt.waitUntil(cachePromise);
+ 
 });
  
 self.addEventListener('activate', (evt) => {
@@ -11,4 +31,25 @@ self.addEventListener('activate', (evt) => {
 self.addEventListener('fetch', (evt) => {
     console.log('sw intercepte la requête suivante via fetch', evt);
     console.log('url interceptée', evt.request.url);
+});
+
+	
+self.addEventListener('fetch', (evt) => {
+    if(!navigator.onLine) {
+        const headers = { headers: { 'Content-Type': 'text/html;charset=utf-8'} };
+        evt.respondWith(new Response('<h1>Pas de connexion internet</h1><div>Application en mode dégradé. Veuillez vous connecter</div>', headers));
+    }
+ 
+    console.log('sw intercepte la requête suivante via fetch', evt);
+    console.log('url interceptée', evt.request.url);
+
+    evt.respondWith(
+        caches.match(evt.request)
+            .then(cachedResponse => {
+                if (cachedResponse) {
+                    return cachedResponse;
+                }
+                return fetch(evt.request);
+            })
+    );
 });
